8f885bb806ea2eb1417f33e55396e7c5bc9e8a88
Benjamin Kwok <benkwokdev@gmail.com>
2017-12-20 16:42:45 -1000

    task action types

--- src/actions/types.js
+++ src/actions/types.js
@@ -3,3 +3,6 @@ export const DEL_BOARD = "DEL_BOARD";
 
 export const ADD_COLUMN = "ADD_COLUMN";
 export const DEL_COLUMN = "DEL_COLUMN";
+
+export const ADD_TASK = "ADD_TASK";
+export const DEL_TASK = "DEL_COLUMN";
