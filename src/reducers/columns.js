35d17277334af6cc8af8a721d7a58435a493dac8
Benjamin Kwok <benkwokdev@gmail.com>
2017-12-23 02:40:23 -1000

    remove console.log

--- src/reducers/columns.js
+++ src/reducers/columns.js
@@ -39,7 +39,6 @@ export default function columns(state = defaultState, action) {
 
 export const addTask = (state, action) => {
   let column = state[action.columnId];
-  console.log("reducer", action)
   return {
     ...state,
     [action.columnId]: {
