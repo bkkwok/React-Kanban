13e0cff3979acf89130663e0329f6ff09a6032a9
Benjamin Kwok <benkwokdev@gmail.com>
2017-12-29 06:36:17 -1000

    fixing folder name bug

--- src/components/Dropdown/DropdownLink.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function DropdownLink(props) {
-  const newProps = { className: "dropdown_link", ...props };
-
-  return <div {...newProps}>{newProps.children}</div>;
-}
