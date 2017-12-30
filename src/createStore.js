a1fcf4033bdff45df6f52eae710cff932aff50cc
Benjamin Kwok <benkwokdev@gmail.com>
2017-12-29 15:06:11 -1000

    persist state in localstorage

--- src/createStore.js
+++ src/createStore.js
@@ -1,13 +1,29 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import reducers from "./reducers";
+import { loadState, saveState } from "./localStorage";
+import throttle from "lodash.throttle";
 
 export default function() {
-  const store = createStore(
-    reducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(thunk)
+  const composeEnhancers =
+    typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(
+          {
+            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+          }
+        )
+      : compose;
+
+  const enhancers = composeEnhancers(applyMiddleware(thunk));
+
+  const persistedState = loadState();
+
+  const store = createStore(reducers, persistedState, enhancers);
+
+  store.subscribe(
+    throttle(() => {
+      saveState(store.getState());
+    }, 1000)
   );
 
   return store;
