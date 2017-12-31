b4724fd10d450ebe9230242b1478eeddb5976841
Benjamin Kwok <benkwokdev@gmail.com>
2017-12-30 19:52:56 -1000

    added tasks to default state

--- src/defaultState.js
+++ src/defaultState.js
@@ -1,30 +1,60 @@
 import uuidv4 from "uuid/v4";
 
+function createIds(num) {
+  const ids = [];
+
+  for(let i = 0; i < num; i++) {
+    ids.push(uuidv4())
+  }
+
+  return ids;
+}
+
 const boardId = uuidv4();
-const colOneId = uuidv4();
-const colTwoId = uuidv4();
+const columnIds = createIds(2);
+const taskIds = createIds(3);
 
 export function createDefaultState() {
+  const timestamp = new Date(Date.now()).toLocaleDateString();
   return {
     boards: {
       [boardId]: {
         id: boardId,
         name: "Untitled board",
-        columns: [colOneId, colTwoId]
+        columns: [columnIds[0], columnIds[1]]
       }
     },
     columns: {
-      [colOneId]: {
-        id: colOneId,
+      [columnIds[0]]: {
+        id: columnIds[0],
         name: "inProgress",
-        tasks: []
+        tasks: [taskIds[0], taskIds[1]]
       },
-      [colTwoId]: {
-        id: colTwoId,
+      [columnIds[1]]: {
+        id: columnIds[1],
         name: "ToDo",
-        tasks: []
+        tasks: [taskIds[2]]
       }
     },
-    tasks: {}
+    tasks: {
+      [taskIds[0]]: {
+        id: taskIds[0],
+        task: 'New UI for dashboard',
+        priority: 'low',
+        timestamp: timestamp
+      },
+      [taskIds[1]]: {
+        id: taskIds[1],
+        task: 'Initial landing page concepts',
+        priority: 'low',
+        timestamp: timestamp
+      },
+      [taskIds[2]]: {
+        id: taskIds[2],
+        task: 'Prepare structural documentation',
+        priority: 'high',
+        timestamp: timestamp
+      }
+    }
   };
 }
